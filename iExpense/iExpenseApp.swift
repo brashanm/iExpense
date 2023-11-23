090685af804fa277f861e82d2524afed0ea8f45f
Brashan <brashanm@gmail.com>
2023-11-23 14:10:42 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Brashan Mohanakumar on 2023-11-23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
