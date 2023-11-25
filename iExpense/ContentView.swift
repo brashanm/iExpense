393499eeaf51918fbf32c978cd8adaead2a03431
Brashan <brashanm@gmail.com>
2023-11-25 00:01:03 -0500

    First

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -4,21 +4,184 @@
 //
 //  Created by Brashan Mohanakumar on 2023-11-23.
 //
-
+import Observation
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//struct thirdView: View {
+//    @Environment(\.dismiss) var dismiss
+//    let name: String
+//    var body: some View {
+//        Button("Dismiss") {
+//            dismiss()
+//        }
+//    }
+//}
+//
+//struct secondView: View {
+//    @State private var showingSheet = false
+//    var body: some View {
+//        Button("Show Third Sheet") {
+//            showingSheet.toggle()
+//        }
+//        .sheet(isPresented: $showingSheet) {
+//            thirdView(name: "@twostraws")
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+////    @State private var showingSheet = false
+//    @State private var numbers = [Int]()
+//    @State private var currentNumber = 1
+//    
+//    
+//    var body: some View {
+//        NavigationStack {
+//            VStack {
+//                List {
+//                    ForEach(numbers, id: \.self) {
+//                        Text("Row \($0)")
+//                    }
+//                    .onDelete(perform: removeRows)
+//                }
+//                
+//                Button("Add Number") {
+//                    numbers.append(currentNumber)
+//                    currentNumber += 1
+//                }
+//            }
+//            .toolbar {
+//                EditButton()
+//            }
+//        }
+//    }
+//    
+//    func removeRows(at offsets: IndexSet) {
+//        numbers.remove(atOffsets: offsets)
+//    }
+//}
+
+//struct User: Codable {
+//    let firstName: String
+//    let lastName: String
+//}
+//
+//struct ContentView: View {
+//    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+//    
+//    var body: some View {
+//        Button("Save User") {
+//            let encoder = JSONEncoder()
+//            
+//            if let data = try? encoder.encode(user) {
+//                UserDefaults.standard.set(data, forKey: "UserData")
+//            }
+//        }
+//    }
+//}
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(expenses.items.filter {$0.type == "Personal"}) { item in
+                        if item.type == "Business" {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
+                                    .foregroundStyle(item.amount < 10 ? .green : (item.amount < 100 ? .yellow : .red))
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                } header: {
+                    Text("Personal")
+                }
+                Section {
+                    ForEach(expenses.items.filter {$0.type == "Business"}) { item in
+                        if item.type == "Business" {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
+                                    .foregroundStyle(item.amount < 10 ? .green : (item.amount < 100 ? .yellow : .red))
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                } header: {
+                    Text("Business")
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Expense", systemImage: "plus") {
+                        showingAddExpense = true
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
